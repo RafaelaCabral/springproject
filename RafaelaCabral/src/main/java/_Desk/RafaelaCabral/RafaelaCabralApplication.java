6ecebd536ab4d6e07a9fceb76cde6590caa446ef
Rafaela Vieira Cabral <50456594+RafaelaCabral@users.noreply.github.com>
2024-08-13 09:42:32 -0300

    Add files via upload

---
+++ RafaelaCabral/src/main/java/_Desk/RafaelaCabral/RafaelaCabralApplication.java
@@ -0,0 +1,13 @@
+package _Desk.RafaelaCabral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafaelaCabralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafaelaCabralApplication.class, args);
+	}
+
+}
---
+++ RafaelaCabral/src/test/java/_Desk/RafaelaCabral/RafaelaCabralApplicationTests.java
@@ -0,0 +1,13 @@
+package _Desk.RafaelaCabral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RafaelaCabralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
