22e41842f154ddfb63fe0c8f50423600edcd0304
Rafaela Vieira Cabral <rafaelavieiracabral04@gmail.com>
2024-08-19 13:28:54 +0000

    update

---
+++ src/main/java/_Desk/RafaelaCabral/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package _Desk.RafaelaCabral.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import _Desk.RafaelaCabral.entity.Usuario;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
+    
+}
